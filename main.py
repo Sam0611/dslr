ec0802cbf3aef0be9899bbff2c054074dc6db635
Sam <mallsam611@gmail.com>
2025-11-27 17:52:01 +0100

    main.py : describe data is working

--- main.py
+++ main.py
@@ -14,13 +14,41 @@ def load(path: str) -> pd.core.frame.DataFrame:
         return None
 
 
+def get_not_empty_values(args: any):
+    """returns every not empty values"""
+    values = []
+    for data in args:
+        if data == data:
+            values.append(data)
+    return values
+
+
 def main():
     try:
         data = load("dataset_train.csv")
-        col = data["Arithmancy"]
-        print("count :", get_count(col))
-        print(data.loc[20])
-        # print(data.describe())
+        numerical_data = data.select_dtypes(include=['number'])
+
+        data_dict = {}
+        for name in numerical_data.columns:
+            col = get_not_empty_values(data[name])
+            data_dict[name] = [
+                get_count(col),
+                get_mean(col),
+                get_standard_deviation(col),
+                get_min(col),
+                get_first_quartile(col),
+                get_median(col),
+                get_third_quartile(col),
+                get_max(col)
+            ]
+
+        labels = ["count", "mean", "std", "min", "25%", "50%", "75%", "max"]
+        df = pd.DataFrame(data_dict, index=labels)
+
+        print(data.describe())
+
+        print(df)
+
     except Exception as error:
         print("Error:", error)
 
