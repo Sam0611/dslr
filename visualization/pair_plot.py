64463bca5dbceff2881f97df241ea81cfb56b379
Sam <mallsam611@gmail.com>
2025-12-08 22:06:51 +0100

    pair plot done

--- visualization/pair_plot.py
+++ visualization/pair_plot.py
@@ -12,40 +12,71 @@ def get_not_empty_values(args: any):
     return values
 
 
+def get_label(str, length):
+    if len(str) <= length:
+        return str
+
+    label = str[:length].rsplit(" ", 1)
+    label = '\n'.join(label) + get_label(str[length:], length)
+    return label
+
+
+def press(event):
+    if event.key == "escape":
+        plt.close()
+
+
 def pair_plot():
     try:
         if len(sys.argv) != 2:
             raise Exception("One argument is required : the path to csv file")
-        
+
         data = pd.read_csv(sys.argv[1])
 
-        numerical_data = data.select_dtypes(include=['number'])
-        numerical_data = numerical_data.drop("Index", axis='columns')
-        if "Hogwarts House" in numerical_data.columns:
-            numerical_data = numerical_data.drop("Hogwarts House", axis='columns')
+        num_data = data.select_dtypes(include=['number'])
+        num_data = num_data.drop("Index", axis='columns')
+        if "Hogwarts House" in num_data.columns:
+            num_data = num_data.drop("Hogwarts House", axis='columns')
 
-        num_features = len(numerical_data.columns)
+        num_features = len(num_data.columns)
 
-        fig, axes = plt.subplots(nrows=num_features, ncols=num_features, figsize=(10, 10))
+        fig, axes = plt.subplots(
+            nrows=num_features,
+            ncols=num_features,
+            figsize=(10, 10)
+        )
 
         for i in range(num_features):
             for j in range(num_features):
-                name1 = numerical_data.columns[i]
-                name2 = numerical_data.columns[j]
+                name1 = num_data.columns[i]
+                name2 = num_data.columns[j]
                 if i == j:
-                    axes[i, j].hist(numerical_data[name1], bins=15, color='Yellow', edgecolor='black')
+                    axes[i, j].hist(
+                        num_data[name1],
+                        bins=15,
+                        color='pink',
+                        edgecolor='black'
+                    )
                 else:
-                    axes[i, j].scatter(numerical_data[name1], numerical_data[name2], color='blue')
+                    axes[i, j].scatter(
+                        num_data[name1],
+                        num_data[name2],
+                        color='orange',
+                        s=1
+                    )
 
                 if j == 0:
-                    axes[i, j].set_ylabel(name1)
+                    axes[i, j].set_ylabel(get_label(name1, 10), fontsize=8)
                 if i == num_features - 1:
-                    axes[i, j].set_xlabel(name2, fontsize=10)
+                    axes[i, j].set_xlabel(get_label(name2, 15), fontsize=8)
 
                 axes[i, j].set_xticks([])
                 axes[i, j].set_yticks([])
 
         plt.tight_layout()
+        manager = plt.get_current_fig_manager()
+        manager.full_screen_toggle()
+        _ = fig.canvas.mpl_connect('key_press_event', press)
         plt.show()
 
     except Exception as error:
