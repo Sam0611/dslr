061770a9ea87471e0a133bbf5edf3ce2554aba62
Sam <mallsam611@gmail.com>
2025-12-18 00:15:43 +0100

    logreg_train first iteration complete for one house

--- regression/logreg_train.py
+++ regression/logreg_train.py
@@ -4,6 +4,15 @@ import pandas as pd
 import numpy as np
 
 
+def replace_nan_values(data, num_data):
+    for col in num_data.columns:
+        for i in range(len(num_data[col])):
+            if math.isnan(num_data[col].loc[i]):
+                house = data.loc[i, 'Hogwarts House']
+                tmp_data = num_data[data['Hogwarts House'].isin([house])]
+                num_data.loc[i, col] = tmp_data[col].mean()
+
+
 def logreg_train():
     """
         takes dataset_train.csv as a parameter
@@ -19,41 +28,43 @@ def logreg_train():
     num_data = num_data.drop("Index", axis='columns')
 
     # replace nan values with the mean for same house
-    # df['A'].fillna(df['A'].mean(), inplace=True)
+    replace_nan_values(data, num_data)
 
     # init variables
     n_rows = len(data['Index'])
-    n_cols = len(num_data.columns)
-    weights = np.zeros(n_cols + 1)
+    n_cols = len(num_data.columns) + 1
+    weights = np.zeros(n_cols)
+    p = np.array([])
+
+    # add column of one
+    num_data.insert(0, 'x0', np.ones(n_rows))
 
     # y = 0|1
-    houses = np.where(data['Hogwarts House'] == 'Gryffindor', 1, 0)
+    y = np.where(data['Hogwarts House'] == 'Gryffindor', 1, 0)
 
-    h = np.array([])
+    # z = w0 + x1*w1 + xn*wn
+    for i in range(n_rows):
+        row = np.array(num_data.loc[i])
+        z = 0
+        for j in range(len(row)):
+            z = z + weights[j] * row[j]
 
-    # xw = w0 + x1*w1 + xn*wn
-    x = np.array(num_data.loc[0])
+        # p = 1 / (1 + e(-z))
+        p = np.append(p, 1 / (1 + math.exp(-z)))
 
-    xw = weights[0]
-    for i in range(len(x)):
-        xw = xw + weights[i + 1] * x[i]
+    # gradient_descent = 1/n * X'(p - y)
+    for col in range(n_cols):
+        gd = 0
+        x = np.array(num_data[num_data.columns[col]])
+        for row in range(n_rows):
+            gd = gd + x[row] * (p[row] - y[row])
+        gd = gd / n_rows
 
-    # h = 1 / (1 + e(-xw))
-    h = np.append(h, 1 / (1 + math.exp(-xw)))
-    
-    print(len(h))
+        # w = w - 0.1 * gd
+        weights[col] = weights[col] - 0.1 * gd
 
-    # gradient_descent = 1/n * X'(h - y)
-
-    # gd1 = 1/n * (h0 - y0 + hn - yn)
-    # gd2 = 1/n * ((h0 - y0) * x0 + (hn - yn) * xn)
-    gradient_descent = h[0]
-    gradient_descent = gradient_descent / n_rows
-
-    # weights = weights - 0.1 * gradient_descent
-
-
-    # loss fct L = -1/n * ( y*log(h) + (1 - y)*log(1 - h) )
+    # loss fct L = -1/n * ( y*log(p) + (1 - y)*log(1 - p) )
+    L = -1 / n_rows * (y[0] * math.log(p[0]) + (1 - y[0]) * math.log(1 - p[0]))
 
 
 def main():
